e5723382d572f75782e615f2c06f6ee60da2bbb0
khanhdzx <khanhvannguyen2303@gmail.com>
2022-07-24 21:46:19 +0700

    ja

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+        //write your code here
+       Date date = new Date();
+       System.out.println("Now is: "+date);
+    }
+
+}
